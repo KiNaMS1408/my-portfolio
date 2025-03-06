5305dd2163171f6a3e15cb5e5d0c231d59420031
Franz Neußer <140851101+KiNaMS1408@users.noreply.github.com>
2025-03-06 18:42:32 +0100

    Image edited in gimp, menu button and light dark mode implemented

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { CommonModule, NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatSlideToggleModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  constructor(private themeService: ThemeService) {}
+  // showToggle = true;
+  isMenuOpen = false;
+
+  constructor(public themeService: ThemeService) {}
 
   toggleTheme(): void {
     this.themeService.toggleTheme();
   }
+  get themeText(): string {
+    return this.themeService.isLightMode ? 'Light Mode' : 'Dark Mode';
+  }
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log(this.isMenuOpen);
+    
+  }
 }
+
--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,28 +1,45 @@
 import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, DOCUMENT } from '@angular/common';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
-  private lightMode = 'light-mode';
+  private lightModeClass = 'light-mode';
+  private darkModeClass = 'dark-mode';
 
-  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+  public isLightMode = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, @Inject(DOCUMENT) private document: Document) {
     this.loadTheme();
   }
 
   toggleTheme(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      document.body.classList.toggle(this.lightMode);
-      localStorage.setItem('theme', document.body.classList.contains(this.lightMode) ? 'light' : 'dark');
+    this.isLightMode = !this.isLightMode;
+    this.updateTheme();
+  }
+
+  private updateTheme(): void {
+    if (this.isLightMode) {
+      this.document.body.classList.add(this.lightModeClass);
+      this.document.body.classList.remove(this.darkModeClass);
+      if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('theme', 'light');
+      }
+    } else {
+      this.document.body.classList.add(this.darkModeClass);
+      this.document.body.classList.remove(this.lightModeClass);
+      if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('theme', 'dark');
+      }
     }
   }
 
   private loadTheme(): void {
     if (isPlatformBrowser(this.platformId)) {
-      const savedTheme = localStorage.getItem('theme');
-      if (savedTheme === 'light') {
-        document.body.classList.add(this.lightMode);
-      }
+    const savedTheme = localStorage.getItem('theme');
+    this.isLightMode = savedTheme === 'light';
+    this.updateTheme();
     }
   }
 }
